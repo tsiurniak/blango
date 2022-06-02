0444412e85c23bcd17ce26b833cbe1ab81931294
Oleksandr Tsiurniak <oleswot@gmail.com>
2022-06-02 19:40:16 +0300

    Finish permissions

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,11 +4,11 @@ from blog.api.views import PostList, PostDetail
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 
+
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
 ]
-
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
